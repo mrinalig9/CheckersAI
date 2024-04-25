92bd8b1f7067beab5d6bccbbe77e927dbd24cf21
Raiyan1000 <bandhan.sazid@gmail.com>
2024-04-24 21:44:54 -0700

    main draws board

--- CheckerGame.py
+++ CheckerGame.py
@@ -3,31 +3,34 @@
 # Game board class
 class CheckerBoard:
     def __init__(self):
-        _BPIECE = 1
-        _WPIECE = 2
-        _BKING = 3
-        _WKING = 4
+        _P1PIECE = 1
+        _P2PIECE = 2
+        _P1KING = 3
+        _P2KING = 4
         # 2D Board Matrix
         # 0 = empty square
-        # 1 = Black piece
-        # 2 = White piece
-        # 3 = Black king
-        # 4 = White king
+        # 1 = Player 1's piece
+        # 2 = Player 2's piece
+        # 3 = Player 1's king
+        # 4 = Player 2's king
         self.board = [
-            [0, _BPIECE, 0, _BPIECE, 0, _BPIECE, 0, _BPIECE],
-            [_BPIECE, 0, _BPIECE, 0, _BPIECE, 0, _BPIECE, 0],
-            [0, _BPIECE, 0, _BPIECE, 0, _BPIECE, 0, _BPIECE],
+            [0, _P1PIECE, 0, _P1PIECE, 0, _P1PIECE, 0, _P1PIECE],
+            [_P1PIECE, 0, _P1PIECE, 0, _P1PIECE, 0, _P1PIECE, 0],
+            [0, _P1PIECE, 0, _P1PIECE, 0, _P1PIECE, 0, _P1PIECE],
             [0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0],
-            [_WPIECE, 0, _WPIECE, 0, _WPIECE, 0, _WPIECE, 0],
-            [0, _WPIECE, 0, _WPIECE, 0, _WPIECE, 0, _WPIECE],
-            [_WPIECE, 0, _WPIECE, 0, _WPIECE, 0, _WPIECE, 0]
+            [_P2PIECE, 0, _P2PIECE, 0, _P2PIECE, 0, _P2PIECE, 0],
+            [0, _P2PIECE, 0, _P2PIECE, 0, _P2PIECE, 0, _P2PIECE],
+            [_P2PIECE, 0, _P2PIECE, 0, _P2PIECE, 0, _P2PIECE, 0]
             ]
-        # indicated player's turn (black starts)
-        self.turn = _BPIECE
+        # indicated player's turn
+        self.turn = _P1PIECE
 
-    # draws the board with pieces onto the screen using pygame
-    def drawBaord(self) -> None:
+        self.player1NumPieces = self.player2NumPieces = 12
+        self.player1NumKings = self.player2NumKings = 0
+
+    # draws the board with pieces onto the window using pygame
+    def drawBaord(self, window) -> None:
         # GUI team works on this
         pass
 
--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import pygame
 from CheckerGame import CheckerBoard
 
+WIDTH = 800
+HEIGHT = 800
+
 if __name__ == "__main__":
+    window = pygame.display.set_mode((WIDTH, HEIGHT))
+    gameActive = True
+    clock = pygame.time.Clock()
+
     CB = CheckerBoard()
     print(CB)
+
+    while gameActive:
+        CB.drawBaord(window)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameActive = False
+            break
+
+    pygame.quit()
+
