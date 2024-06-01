08e5ef568fc4ee8b28538ca8a4ab0332766dd7dd
Shelby <91756935+smcorpuz@users.noreply.github.com>
2024-05-31 21:08:41 -0700

    Update main.py
    Co-authored-by: Raiyan1000 <32380335+araiyan@users.noreply.github.com>

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from CheckerAI import CheckerAI
 from Transition import BoardTransition
 from constants import HEIGHT, WIDTH, _P1PIECE, _P2PIECE, _FORCED_CAPTURE
 
-font = pygame.font.SysFont('Arial', 24)
 
 if __name__ == "__main__":
     window = pygame.display.set_mode((WIDTH, HEIGHT))
